5bfd0cc0102230202371e17ceaae9f3fe30fc48b
Spencer <spencer@dhcp-v042-100.mobile.uci.edu>
2023-11-14 15:24:49 -0800

    Retrying the main git with a new branch

--- SchlierenVideoProcessing_avi.m
+++ SchlierenVideoProcessing_avi.m
@@ -1,6 +1,9 @@
 %% X Energy Lab Schlieren Video Processing Code V1.0.1 (avi edition)
 % Last Update: 11/13/23 by Spencer K
 %
+% Adding in another dummy line
+% 
+% 
 % This version takes in a .avi video recorded in gray scale. This version
 % also only manipulates 8bit data. Still working on how to get the 12
 % videos going. But thats still a work in progress
